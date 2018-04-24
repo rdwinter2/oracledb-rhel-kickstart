09764b9d51c76c629323c09a1122b92d12264e58
Robert Winter <rdwinter2@gmail.com>
2018-04-24 14:18:48 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+cd /opt/ansible
+. /opt/ansible/hacking/env-setup -q
+cd $OLDPWD
+
+/opt/ansible/bin/ansible-playbook -v \
+    ${WORKSPACE}/play.yml \
+    -i ${WORKSPACE}/inventory \
+    --forks 5 --syntax-check
+    
+/opt/ansible/bin/ansible-playbook -v \
+    ${WORKSPACE}/play.yml \
+    -i ${WORKSPACE}/inventory \
+    --forks 5
+    
+    
+/opt/ansible/bin/ansible-playbook -v \
+    ${WORKSPACE}/play.yml \
+    -i ${WORKSPACE}/inventory \
+    --forks 5 \
+    | grep -q 'changed=0.*failed=0' \
+    && (echo 'Idempotence test: pass' && exit 0) \
+    || (echo 'Idempotence test: fail' && exit 1)
